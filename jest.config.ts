537247c1192af5f0a843bc00ecd210059ebd8788
Sébastien Wauquier <sebastien.wauquier@dashlane.com>
2024-02-28 14:22:25 +0100

    chore: upgrade to jest@29

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,6 @@
 import type { InitialOptionsTsJest } from "ts-jest/dist/types";
 
 const config: InitialOptionsTsJest = {
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.json",
-        },
-    },
     moduleFileExtensions: ["ts", "js"],
     preset: "ts-jest",
     rootDir: "test",
